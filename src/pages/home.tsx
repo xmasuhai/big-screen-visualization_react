0362e1481a693785a6ad3f5a90a9cb158228d8af
xmasuhai <xmasuhai@163.com>
2023-03-22 20:28:04 +0800

    修复公式计算错误；添加头部样式

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss';
+import  headerBg from '../images/header.png';
 
 export const Home = () => {
   return (
-    <div>
-      <div className="x"></div>
+    <div className="home">
+      <header style={{backgroundImage: `url(${headerBg})`}}></header>
     </div>
   );
 };
