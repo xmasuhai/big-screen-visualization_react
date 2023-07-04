cca0d91dc45f1dd7991eb697d0b2bb143a3c3c14
xmasuhai <xmasuhai@163.com>
2023-07-04 20:21:05 +0800

    初步实现 chart2
    Took 10 minutes

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -13,36 +13,48 @@ export const Chart2 = () => {
       echarts.init(divRef.current)
     // 绘制图表
     myChart.setOption(createEchartsOptions({
-      xAxis: {
-        type: 'value',
-        boundaryGap: [0, 0.01]
-      },
-      yAxis: {
-        type: 'category',
-        data: ['Brazil', 'Indonesia', 'USA', 'India', 'China', 'World']
-      },
-      series: [
-        {
-          name: '2011',
-          type: 'bar',
-          data: [18203, 23489, 29034, 104970, 131744, 630230]
+        xAxis: {
+          type: 'value',
+          boundaryGap: [0, 0.01],
+          splitLine: {show: false},
+          axisLabel: {show: false}
         },
-        {
-          name: '2012',
-          type: 'bar',
-          data: [19325, 23438, 31000, 121594, 134141, 681807]
-        }
-      ]
-
-    })
+        yAxis: {
+          axisTick: {show: false},
+          type: 'category',
+          data: ['城关区公安局', '七里河区公安局', '西固区公安局', '安宁区公安局', '红古区公安局',
+            '永登县公安局', '皋兰县公安局', '榆中县公安局', '新区公安局'],
+          axisLabel: {
+            formatter(val) {
+              return val.replace('公安局', '\n公安局');
+            }
+          }
+        },
+        series: [
+          {
+            name: '2011 年',
+            type: 'bar',
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+          },
+          {
+            name: '2012 年',
+            type: 'bar',
+            data: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+          }
+        ]
+      })
     )
 
   }, [])
 
   return (
-    <div className="bordered jurisdictional-statistics">
-      <h2> 按键破获排名 </h2>
-      <div ref={divRef} className="chart"></div>
+    <div className="bordered 破获排名">
+      <h2>案件破获排名</h2>
+      <div ref={divRef} className="chart"/>
+      <div className="legend">
+        <span className="first"/> 破案排名 1
+        <span className="second"/> 破案排名 1
+      </div>
     </div>
   )
 }
