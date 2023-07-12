2c989b7fa2f133bd5234d9d9455f31839bab2763
xmasuhai <xmasuhai@163.com>
2023-07-13 06:26:11 +0800

    添加渐变
    Took 39 minutes

--- src/components/chart-2.tsx
+++ src/components/chart-2.tsx
@@ -34,12 +34,28 @@ export const Chart2 = () => {
           {
             name: '2011 年',
             type: 'bar',
-            data: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+            data: [1, 2, 3, 4, 5, 6, 7, 8, 9],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0,[
+                  {offset: 0, color: '#2034f9'},
+                  {offset: 1, color: '#04a1ff'},
+                ]),
+              }
+            },
           },
           {
             name: '2012 年',
             type: 'bar',
-            data: [2, 3, 4, 5, 6, 7, 8, 9, 10]
+            data: [2, 3, 4, 5, 6, 7, 8, 9, 10],
+            itemStyle: {
+              normal: {
+                color: new echarts.graphic.LinearGradient(0, 0, 1, 0,[
+                  {offset: 0, color: '#b92aE8'},
+                  {offset: 1, color: '#6773E7'},
+                ]),
+              }
+            },
           }
         ]
       })
