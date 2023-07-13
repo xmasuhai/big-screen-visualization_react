f48fe70938071972a0a9b00ab77e3efcf7fa39f1
xmasuhai <xmasuhai@163.com>
2023-07-14 06:22:00 +0800

    制作chart-3
    Took 29 minutes

---
+++ src/components/chart-3.tsx
@@ -0,0 +1,91 @@
+import React, {useEffect, useRef} from 'react'
+import * as echarts from 'echarts'
+import {createEchartsOptions} from '../shared/create-echarts-options'
+import {px} from '../shared/px'
+
+export const Chart3 = () => {
+  const divRef = useRef(null)
+
+  useEffect(() => {
+    const myChart = echarts.init(divRef.current)
+    myChart.setOption(createEchartsOptions({
+      // 标题说明文字
+      legend: {
+        bottom: px(10),
+        textStyle: {color: 'white'},
+        itemWidth: px(30),
+        itemHeight: px(16)
+      },
+      // 四周空隙
+      grid: {
+        x: px(20),
+        x2: px(20),
+        y: px(20),
+        y2: px(70),
+        containLabel: true
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018],
+        // 网格线
+        splitLine: {show: true, lineStyle: {color: '#073E78'}},
+        // 轴刻度标识
+        axisTick: {show: false},
+        axisLine: {show: false},
+      },
+      yAxis: {
+        type: 'value',
+        splitLine: {lineStyle: {color: '#073E78'}},
+        axisLabel: {
+          formatter(val) {
+            return val * 100 + '%'
+          }
+        }
+      },
+      // 主要数据
+      series: [
+        {
+          name: '抢劫',
+          type: 'line',
+          data: [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09].reverse()
+        },
+        {
+          name: '醉驾',
+          type: 'line',
+          data: [0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10].reverse()
+        },
+        {
+          name: '盗窃',
+          type: 'line',
+          data: [0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11].reverse()
+        },
+        {
+          name: '故意杀人',
+          type: 'line',
+          data: [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12].reverse()
+        },
+        {
+          name: '故意伤人',
+          type: 'line',
+          data: [0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.11, 0.12, 0.13].reverse()
+        }
+      ]
+        // 节点圆球
+        .map(obj => ({
+          ...obj,
+          symbol: 'circle',
+          symbolSize: px(12),
+          lineStyle: {width: px(2)}
+        }))
+    }))
+
+  }, [])
+
+  return (
+    <div className="bordered trends-in-crime">
+      <h2>发案趋势分析</h2>
+      <div ref={divRef} className="chart"/>
+    </div>
+  )
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,6 +3,7 @@ import './home.scss'
 import headerBg from '../images/header.png'
 import {Chart1} from '../components/chart-1'
 import {Chart2} from '../components/chart-2'
+import {Chart3} from '../components/chart-3'
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -14,7 +15,8 @@ export const Home = () => {
           <Chart1/>
           <Chart2/>
         </section>
-        <section className="bordered section2">
+        <section className="section2">
+          <Chart3/>
         </section>
         <section className="bordered section3"></section>
         <section className="bordered section4"></section>
