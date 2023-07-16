91fabe1034ecbce064e8038e1e63da9aff356330
xmasuhai <xmasuhai@163.com>
2023-07-16 22:31:18 +0800

    制作 chart-5
    Took 18 minutes

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,6 +5,7 @@ import {Chart1} from '../components/chart-1'
 import {Chart2} from '../components/chart-2'
 import {Chart3} from '../components/chart-3'
 import {Chart4} from "../components/chart-4";
+import {Chart5} from "../components/chart-5";
 
 export const Home = () => {
   const year = new Date().getFullYear();
@@ -20,7 +21,9 @@ export const Home = () => {
           <Chart3/>
           <Chart4/>
         </section>
-        <section className="bordered section3"></section>
+        <section className="bordered section3">
+          <Chart5/>
+        </section>
         <section className="bordered section4"></section>
         <section className="bordered section5"></section>
       </main>
