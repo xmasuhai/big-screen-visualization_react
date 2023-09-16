e4d92785b8586de23e645c72c1ed9e1b7aefae85
xmasuhai <xmasuhai@163.com>
2023-09-16 13:13:12 +0800

    添加 大屏可视化项目 README.md，移除 react body，重装依赖
    Took 32 minutes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './shared/style.scss'
-import {App} from './app'
-import {DevSupport} from '@react-buddy/ide-toolbox'
-import {ComponentPreviews, useInitial} from './dev'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './shared/style.scss';
+import {App} from './app';
 
 ReactDOM.render(
-  <DevSupport ComponentPreviews={ComponentPreviews}
-              useInitialHook={useInitial}
-  >
-    <App/>
-  </DevSupport>,
+  <App/>,
   document.getElementById('root')
-)
+);
