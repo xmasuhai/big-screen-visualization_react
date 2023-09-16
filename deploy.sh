550bd2d97a1dd8a4f7480f2cdcb1dbd716401368
xmasuhai <xmasuhai@163.com>
2023-09-16 20:10:19 +0800

    👷 添加部署脚本
    Took 3 minutes

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m deploy &&
-git remote add origin git@github.com:xmasuhai/big-screen-demo-website-1.git &&
-git remote add origin-1 git@gitee.com:xmasuhai/big-screen-demo-1.git &&
 git push -u -f origin master &&
+git push -u -f origin-1 master &&
 cd ..
