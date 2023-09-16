6a16a28832de181a56f8f3210e33f1ac754df6a2
xmasuhai <xmasuhai@163.com>
2023-09-16 20:26:10 +0800

    👷 部署脚本
    Took 14 minutes

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+#!/usr/bin/env sh
+rm -rf dist &&
 yarn build --base ./ &&
 cd dist &&
 git init &&
 git add . &&
-git commit -m deploy &&
+git commit -m 'deploy' &&
+git remote add origin git@gitee.com:xmasuhai/big-screen-demo-website-1.git &&
+git remote add origin-2 git@github.com:xmasuhai/big-screen-demo-website-1.git &&
 git push -u -f origin master &&
-git push -u -f origin-1 master &&
+git push -u -f origin-2 master &&
 cd ..
+echo https://xmasuhai.xyz/big-screen-demo-website-1/#/
