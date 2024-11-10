ff5f271d0cde489ac2324163df2008aa35b23675
xmasuhai <xmasuhai@163.com>
2024-11-10 08:51:59 +0800

    Update dependencies in yarn.lock
    Refreshed dependencies and updated lockfile to latest versions including several Babel and esbuild packages. This change ensures compatibility with the latest features and fixes.
    
    Took 11 minutes

--- src/pages/config.tsx
+++ src/pages/config.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React from 'react'
+import {Link} from 'react-router-dom'
 
-export const Config = () => {
+export const Config: React.FC = () => {
   return (
     <div>
       配置页面
       <Link to="/">返回首页</Link>
     </div>
-  );
-};
+  )
+}
